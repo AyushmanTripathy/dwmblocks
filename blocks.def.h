65645d122cb0f5d02cf51f08a39625f650845c89
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2023-06-08 17:25:54 +0530

    added bluetooth

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "memory",	30,		0 },
-  {"", "cpu-temp", 5,   0 },
-  {"", "cpu-load", 1,   0 },
-  {"", "get-volume", 0, 10},
-  {"", "battery", 5,    0 },
-	{"", "date '+%b %d (%a) %I:%M%p'", 60,   0},
+  {"", "get-bluetooth-device-info", 0, 12   }, 
+  {"", "nettraf",                   3,  0   },
+	{"", "memory",	                  30, 0   },
+  {"", "cpu-temp",                  5,  0   },
+  {"", "cpu-load",                  1,  0   },
+  {"", "brightness -g",             0,  11  },
+  {"", "get-volume",                0,  10  },
+  {"", "battery",                   5,  0   },
+	{"", "date '+%b %d (%a) %I:%M%p'", 0,   9 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
